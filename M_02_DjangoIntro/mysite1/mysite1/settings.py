cd575760a48bb0baad27ac385670f1827b37377a
Шалайко Дмитрий <demon_at@mail.ru>
2023-06-13 23:30:04 +0300

    создание приложения

--- M_02_DjangoIntro/mysite1/mysite1/settings.py
+++ M_02_DjangoIntro/mysite1/mysite1/settings.py
@@ -47,6 +47,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'shpopapp.apps.ShopappConfig ',
 ]
 
 ROOT_URLCONF = 'mysite1.urls'
