0b2f813bc8689bea64512605314a7d50872a313c
Шалайко Дмитрий <demon_at@mail.ru>
2023-06-13 23:53:59 +0300

    создание приложения

--- M_02_DjangoIntro/mysite1/mysite1/settings.py
+++ M_02_DjangoIntro/mysite1/mysite1/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'shopapp.apps.ShopappConfig',
 ]
 
 MIDDLEWARE = [
@@ -47,7 +48,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'shpopapp.apps.ShopappConfig ',
 ]
 
 ROOT_URLCONF = 'mysite1.urls'
--- M_02_DjangoIntro/mysite1/shopapp/views.py
+++ M_02_DjangoIntro/mysite1/shopapp/views.py
@@ -1,3 +1,15 @@
+from django.http import HttpResponse, HttpRequest
 from django.shortcuts import render
+from timeit import default_timer
 
-# Create your views here.
+def shop_index(request: HttpRequest):
+    products = [
+        ('Ноутбук', 2000),
+        ('Компьютер', 2500),
+        ('Смартфон', 1100),
+    ]
+    context = {
+        'time_running': default_timer(),
+        'products': products,
+    }
+    return render(request, 'shopaap/shop_index.html', context= context)
