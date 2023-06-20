4fba100e707e52b71a4203b26dc7ad7289835072
Шалайко Дмитрий <demon_at@mail.ru>
2023-06-20 21:26:24 +0300

    Создал новые поля в модели Products и вывел их на экран

--- M_03_DatabaseAndModels/mysite/shopapp/views.py
+++ M_03_DatabaseAndModels/mysite/shopapp/views.py
@@ -2,6 +2,7 @@ from django.http import HttpResponse, HttpRequest
 from django.shortcuts import render
 from timeit import default_timer
 from django.contrib.auth.models import Group
+from  .models import Product
 
 def shop_index(request: HttpRequest):
     products = [
@@ -20,3 +21,9 @@ def groups_list(request: HttpRequest):
         'groups': Group.objects.all(),
     }
     return render(request, 'shopapp/groups_list.html', context= context)
+
+def products_list(request: HttpRequest):
+    context = {
+        'products' : Product.objects.all(),
+    }
+    return render(request, 'shopapp/products-list.html', context= context)
