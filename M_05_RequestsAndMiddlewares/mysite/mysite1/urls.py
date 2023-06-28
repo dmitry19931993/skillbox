6f18a9f6c099b476a5fbd59598223a8cb172fa1d
Шалайко Дмитрий <demon_at@mail.ru>
2023-06-28 23:17:15 +0300

    создал  view-функцию для загрузки файлов с ограничением по размеру

--- M_05_RequestsAndMiddlewares/mysite/mysite1/urls.py
+++ M_05_RequestsAndMiddlewares/mysite/mysite1/urls.py
@@ -20,5 +20,7 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('shop/', include('shopapp.urls')),
+    path('req/', include('requestdataapp.urls')),
+    path('ai/', include('app.urls')),
 ]
 
