2c9a9a9a647f0482b0454066a637f13b296ec11a
Шалайко Дмитрий <demon_at@mail.ru>
2023-06-30 23:56:16 +0300

    перенес из модуля 6

--- M_07_CBV/mysite/shopapp/models.py
+++ M_07_CBV/mysite/shopapp/models.py
@@ -5,6 +5,8 @@ from django.db import models
 class Product(models.Model):
     class Meta:
         ordering = ["name", "price"]
+        # db_table = "tech_products"
+        # verbose_name_plural = "products"
 
     name = models.CharField(max_length=100)
     description = models.TextField(null=False, blank=True)
@@ -12,6 +14,12 @@ class Product(models.Model):
     discount = models.SmallIntegerField(default=0)
     created_at = models.DateTimeField(auto_now_add=True)
     archived = models.BooleanField(default=False)
+
+    # @property
+    # def description_short(self):
+    #     if len(self.description) < 50:
+    #         return self.description
+    #     return self.description[:48] + "..."
 
     def __str__(self):
         return f"Product(pk={self.pk}, name={self.name!r})"
