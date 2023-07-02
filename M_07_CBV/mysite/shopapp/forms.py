ef935b2e87897b751494e0f46ab9a273f90215e5
Шалайко Дмитрий <demon_at@mail.ru>
2023-07-02 16:13:59 +0300

    Создал списки товаров через view функцию

--- M_07_CBV/mysite/shopapp/forms.py
+++ M_07_CBV/mysite/shopapp/forms.py
@@ -1,13 +1,19 @@
-from django import forms
 from .models import Product, Order
+from django.contrib.auth.models import Group
+from django.forms import ModelForm
 
-class ProductForm(forms.ModelForm):
+class ProductForm(ModelForm):
     class Meta:
         model = Product
         fields = "name", "price", "description", "discount"
 
 
-class OrderForm(forms.ModelForm):
+class OrderForm(ModelForm):
     class Meta:
         model = Order
         fields = "user", "delivery_address", "promocode", "products"
+
+class GroupForm(ModelForm):
+    class Meta:
+        model = Group
+        fields = ("name",)
