123be98341896f9b2505bdc149eadb13571e69c5
Шалайко Дмитрий <demon_at@mail.ru>
2023-07-03 16:34:02 +0300

    с помощью UpdateView и DeleteView вывел обновление и удаление продуктов

--- M_07_CBV/mysite/shopapp/urls.py
+++ M_07_CBV/mysite/shopapp/urls.py
@@ -7,7 +7,9 @@ from .views import (
     ProductListView,
     OrderDetailView,
     OrderListView,
-    create_product,
+    ProductCreateView,
+    ProductUpdateView,
+    ProductDeleteView,
     create_order
 )
 
@@ -18,7 +20,9 @@ urlpatterns = [
     path("groups/", GroupsListView.as_view(), name="groups_list"),
     path("products/", ProductListView.as_view(), name="products_list"),
     path("products/<int:pk>/", ProductDetailView.as_view(), name="products_detail"),
-    path("products/create", create_product, name="create-product"),
+    path("products/<int:pk>/update", ProductUpdateView.as_view(), name="products_update"),
+    path("products/<int:pk>/archive", ProductDeleteView.as_view(), name="products_archive"),
+    path("products/create", ProductCreateView.as_view(), name="create-product"),
     path("orders/", OrderListView.as_view(), name="orders_list"),
     path("orders/create", create_order, name="create-order"),
     path("orders/<int:pk>/", OrderDetailView.as_view(), name="orders_detail"),
--- M_07_CBV/mysite/shopapp/views.py
+++ M_07_CBV/mysite/shopapp/views.py
@@ -1,10 +1,10 @@
 from timeit import default_timer
 
 from django.contrib.auth.models import Group
-from django.http import HttpResponse, HttpRequest
-from django.shortcuts import render, redirect, reverse, get_object_or_404
-from django.views.generic import TemplateView, ListView, DetailView
-
+from django.http import HttpResponse, HttpRequest, HttpResponseRedirect
+from django.shortcuts import render, redirect, reverse
+from django.urls import reverse_lazy
+from django.views.generic import TemplateView, ListView, DetailView, CreateView, UpdateView, DeleteView
 from .forms import ProductForm, OrderForm, GroupForm
 from .models import Product, Order
 from django.views import View
@@ -46,29 +46,39 @@ class ProductDetailView(DetailView):
 
 class ProductListView(ListView):
     template_name = 'shopapp/products-list.html'
-    model = Product
+    queryset = Product.objects.filter(archived=False)
     context_object_name = 'products'
 
-def create_product(request: HttpRequest):
-    if request.method == 'POST':
-        form = ProductForm(request.POST)
-        if form.is_valid():
-            #Product.objects.create(**form.cleaned_data)
-            form.save()
-            url = reverse("shopapp:products_list")
-            return redirect(url)
-    else:
-        form = ProductForm()
-    context = {
-        "form": form,
-    }
-    return render(request, 'shopapp/create-product.html', context=context)
+class ProductCreateView(CreateView):
+    model = Product
+    form_class = ProductForm
+    success_url = reverse_lazy('shopapp:products_list')
+
+class ProductUpdateView(UpdateView):
+    model = Product
+    fields = "name", "price", "description", "discount",
+    template_name_suffix = "_update_form"
+
+    def get_success_url(self):
+        return reverse(
+            "shopapp:products_detail",
+            kwargs={"pk" : self.object.pk},
+        )
+
+class ProductDeleteView(DeleteView):
+    model = Product
+    success_url = reverse_lazy('shopapp:products_list')
+
+    def form_valid(self, form):
+        success_url = self.get_success_url()
+        self.object.archived = True
+        self.object.save()
+        return HttpResponseRedirect(success_url)
 
 def create_order(request: HttpRequest):
     if request.method == 'POST':
         form = OrderForm(request.POST)
         if form.is_valid():
-            #Product.objects.create(**form.cleaned_data)
             form.save()
             url = reverse("shopapp:orders_list")
             return redirect(url)
