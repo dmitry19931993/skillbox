9dfc8acf1b9e401e7e2b06f976c3bb582f114f34
Шалайко Дмитрий <demon_at@mail.ru>
2023-07-23 23:50:50 +0300

    готовое решение

--- M_11_Files/mysite/shopapp/forms.py
+++ M_11_Files/mysite/shopapp/forms.py
@@ -1,19 +1,22 @@
 from .models import Product, Order
 from django.contrib.auth.models import Group
-from django.forms import ModelForm
-
-class ProductForm(ModelForm):
+from django import forms
+class ProductForm(forms.ModelForm):
     class Meta:
         model = Product
         fields = "name", "price", "description", "discount", "preview",
 
+    images = forms.ImageField(
+        widget=forms.ClearableFileInput(attrs={"allow_multiple_selected":True}),
+    )
 
-class OrderForm(ModelForm):
+
+class OrderForm(forms.ModelForm):
     class Meta:
         model = Order
         fields = "user", "delivery_address", "promocode", "products"
 
-class GroupForm(ModelForm):
+class GroupForm(forms.ModelForm):
     class Meta:
         model = Group
         fields = ("name",)
