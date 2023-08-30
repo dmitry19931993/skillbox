19a92b0baf60d31bf2e30db3a10554431cced785
Шалайко Дмитрий <demon_at@mail.ru>
2023-08-31 00:11:02 +0300

    Изменил функцию lastmode()

--- import_export/mysite/shopapp/sitemap.py
+++ import_export/mysite/shopapp/sitemap.py
@@ -3,11 +3,11 @@ from django.contrib.sitemaps import Sitemap
 from .models import Product
 
 class ShopSitemap(Sitemap):
-    changefriq = "never"
+    changefriq = "always"
     priority = 0.5
     def items(self):
         return (
             Product.objects.filter(name__contains="Phone")
         )
     def lastmod(self, obj: Product):
-        return obj.name
+        return obj.created_at
